bc9c266aee0f1a1e62d2353a2191a4ae343dcdc7
Carolina Melendez <carolina.melendez@bolivia.assuresoft.com>
2024-03-12 14:39:01 -0400

    chore: delete unnecessary imports

--- src/main/java/assuresoft/com/sciencemuseum2/Execute.java
+++ src/main/java/assuresoft/com/sciencemuseum2/Execute.java
@@ -1,13 +1,6 @@
 package assuresoft.com.sciencemuseum2;
 
 import assuresoft.com.sciencemuseum2.animals.Cougar;
-import assuresoft.com.sciencemuseum2.systems.digestive.CarnivorousDigestion;
-import assuresoft.com.sciencemuseum2.systems.reproductive.SexualReproduction;
-import assuresoft.com.sciencemuseum2.systems.respiratory.LungRespiration;
-import assuresoft.com.sciencemuseum2.systems.sensorial.Sensors;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Execute {
     public static void main(String[] args) {
