3fbcecec03e27d146eaff765487429dc75125158
JuanPabloTraining <juanpablo.carrasco@training.assuresoft.com>
2024-03-14 17:05:48 -0400

    Fixing observations

--- src/main/java/assuresoft/com/sciencemuseum2/ExecuteWolfPack.java
+++ src/main/java/assuresoft/com/sciencemuseum2/ExecuteWolfPack.java
@@ -2,34 +2,22 @@ package assuresoft.com.sciencemuseum2;
 
 import assuresoft.com.sciencemuseum2.animals.Wolf;
 
-import java.security.KeyStore;
-
-public class Execute2 {
+public class ExecuteWolfPack {
     public static void main(String[] args) {
         Wolf maleWolf = new Wolf("Canis lupus","Wolf1", "Male",
                 "White", true);
-        maleWolf.getAnimalInformation();
-        maleWolf.performLifeProcesses();
 
-        Wolf femaleWolf = new Wolf("Canis lupus","Wolf2", "Male",
+        Wolf femaleWolf = new Wolf("Canis lupus","Wolf2", "Female",
                 "White", false);
-        femaleWolf.getAnimalInformation();
-        femaleWolf.performLifeProcesses();
 
         Wolf wolf1 = new Wolf("Canis lupus","Wolf3", "Male",
                 "White", false);
-        wolf1.getAnimalInformation();
-        wolf1.performLifeProcesses();
 
         Wolf wolf2 = new Wolf("Canis lupus","Wolf4", "Male",
                 "White", true);
-        wolf2.getAnimalInformation();
-        wolf2.performLifeProcesses();
 
         Wolf wolf3 = new Wolf("Canis lupus","Wolf5", "Male",
                 "White", false);
-        wolf3.getAnimalInformation();
-        wolf3.performLifeProcesses();
 
         WolfPack wolfpack = new WolfPack(maleWolf, femaleWolf);
         wolfpack.addWolfToPack(wolf1);
--- src/main/java/assuresoft/com/sciencemuseum2/WolfPack.java
+++ src/main/java/assuresoft/com/sciencemuseum2/WolfPack.java
@@ -1,7 +1,6 @@
 package assuresoft.com.sciencemuseum2;
 
 import assuresoft.com.sciencemuseum2.animals.Wolf;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,13 +13,16 @@ public class WolfPack{
 
     public void addWolfToPack(Wolf newWolf){
         System.out.println("Checking if wolf is Alpha...");
+
         if (newWolf.isAlpha()){
             System.out.println("The wolf is alpha! He will fight with the Wolf Pack's Alpha now.");
             System.out.println("The wolf lost, now he will not be considered alpha anymore.");
             newWolf.setAlpha(false);
-        }else {
+        }
+        else {
             System.out.println("The wolf is not alpha, he will have no problem joining the Wolf Pack.");
         }
+
         System.out.println("The wolf has joined the Wolf Pack.\n");
         wolfPackMembers.add(newWolf);
         newWolf.joinWolfPack(this);
