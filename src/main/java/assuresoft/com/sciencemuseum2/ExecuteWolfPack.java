8cdb0f76d3632d891fca2926c1edbf8ba63809cd
JuanPabloTraining <juanpablo.carrasco@training.assuresoft.com>
2024-03-18 15:43:21 -0400

    Clean code update

--- src/main/java/assuresoft/com/sciencemuseum2/ExecuteWolfPack.java
+++ src/main/java/assuresoft/com/sciencemuseum2/ExecuteWolfPack.java
@@ -3,21 +3,23 @@ package assuresoft.com.sciencemuseum2;
 import assuresoft.com.sciencemuseum2.animals.Wolf;
 
 public class ExecuteWolfPack {
+    private static final String SCIENTIFIC_NAME = "Canis lupus";
+    private static final String FUR_COLOR = "White";
     public static void main(String[] args) {
-        Wolf maleWolf = new Wolf("Canis lupus","Wolf1", "Male",
-                "White", true);
+        Wolf maleWolf = new Wolf(SCIENTIFIC_NAME,"Wolf1", "Male",
+                FUR_COLOR, true);
 
-        Wolf femaleWolf = new Wolf("Canis lupus","Wolf2", "Female",
-                "White", false);
+        Wolf femaleWolf = new Wolf(SCIENTIFIC_NAME,"Wolf2", "Female",
+                FUR_COLOR, false);
 
-        Wolf wolf1 = new Wolf("Canis lupus","Wolf3", "Male",
-                "White", false);
+        Wolf wolf1 = new Wolf(SCIENTIFIC_NAME,"Wolf3", "Male",
+                FUR_COLOR, false);
 
-        Wolf wolf2 = new Wolf("Canis lupus","Wolf4", "Male",
-                "White", true);
+        Wolf wolf2 = new Wolf(SCIENTIFIC_NAME,"Wolf4", "Male",
+                FUR_COLOR, true);
 
-        Wolf wolf3 = new Wolf("Canis lupus","Wolf5", "Male",
-                "White", false);
+        Wolf wolf3 = new Wolf(SCIENTIFIC_NAME,"Wolf5", "Male",
+                FUR_COLOR, false);
 
         WolfPack wolfpack = new WolfPack(maleWolf, femaleWolf);
         wolfpack.addWolfToPack(wolf1);
