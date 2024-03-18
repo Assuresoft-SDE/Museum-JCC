d9137819466e345ff428f7f6c55e76889d10f462
CatherineGomez-AD <catherine.gomez@training.assuresoft.com>
2024-03-18 16:51:54 -0400

    Magic numbers assigned to variables

--- src/main/java/assuresoft/com/sciencemuseum2/FibonacciSearch.java
+++ src/main/java/assuresoft/com/sciencemuseum2/FibonacciSearch.java
@@ -2,6 +2,9 @@ package assuresoft.com.sciencemuseum2;
 import java.util.*;
 
 public class FibonacciSearch {
+    private static final int NOT_FOUND = -1;
+    private static final int PENULTIMATE_FIBONACCI = 0;
+    private static final int PREVIOUS_FIBONACCI = 1;
 
     private static int checkValidLocation(int indexToCheck, int maxIndex) {
         return Math.min(indexToCheck, maxIndex);
@@ -9,8 +12,8 @@ public class FibonacciSearch {
 
     public static int fibonacciSearch(List<Integer> list, int searchItem) {
         int listSize = list.size();
-        int penultimateFibonacci = 0;
-        int previousFibonacci = 1;
+        int penultimateFibonacci = PENULTIMATE_FIBONACCI;
+        int previousFibonacci = PREVIOUS_FIBONACCI;
         int currentFibonacci = penultimateFibonacci + previousFibonacci;
 
         // Find the fibonacci number that is equal or major to the array's size
@@ -20,7 +23,7 @@ public class FibonacciSearch {
             currentFibonacci = penultimateFibonacci + previousFibonacci;
         }
 
-        int eliminatedFront = -1;
+        int eliminatedFront = NOT_FOUND;
 
         while (currentFibonacci > 1) {
             int index = checkValidLocation(eliminatedFront + penultimateFibonacci, listSize - 1);
@@ -40,11 +43,21 @@ public class FibonacciSearch {
                 }
         }
 
-        if (previousFibonacci == 1 && list.get(listSize - 1) == searchItem) {
+        if (previousFibonacci == PREVIOUS_FIBONACCI && list.get(listSize - 1) == searchItem) {
             return listSize - 1;
         }
 
-        return -1;
+        return NOT_FOUND;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(10, 22, 35, 40, 45, 50, 80, 82, 85, 90, 100);
+        int x = 85;
+        int index = fibonacciSearch(list, x);
+        if (index >= 0)
+            System.out.print("Found element " + list. get(index) + " at index: " + index);
+        else
+            System.out.print(x + " isn't present in the list");
     }
 }
 
