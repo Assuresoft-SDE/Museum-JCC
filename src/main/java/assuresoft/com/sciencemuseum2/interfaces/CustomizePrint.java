e4969b93b1f60a59a15a8116c1022315d4d3dc00
CatherineGomez-AD <catherine.gomez@training.assuresoft.com>
2024-03-20 18:06:14 -0400

    block comment added to CustomizePrint interface

--- src/main/java/assuresoft/com/sciencemuseum2/interfaces/CustomizePrint.java
+++ src/main/java/assuresoft/com/sciencemuseum2/interfaces/CustomizePrint.java
@@ -1,6 +1,9 @@
 package assuresoft.com.sciencemuseum2.interfaces;
 
 public interface CustomizePrint {
+    /**
+     * @param message The message to be displayed.
+     */
     default public void displayMessage(String message){
         System.out.println("\t- " + message);
     }
