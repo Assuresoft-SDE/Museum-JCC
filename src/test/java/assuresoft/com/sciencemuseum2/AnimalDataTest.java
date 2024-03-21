f1c795f241c14ed8a86ccb4b11e96d94bd382367
JuanPabloTraining <juanpablo.carrasco@training.assuresoft.com>
2024-03-20 21:38:57 -0400

    test: created AnimalSystems unit tests

---
+++ src/test/java/assuresoft/com/sciencemuseum2/AnimalDataTest.java
@@ -0,0 +1,33 @@
+package assuresoft.com.sciencemuseum2;
+
+import junit.framework.TestCase;
+
+public class AnimalDataTest extends TestCase {
+
+    public void testGetAnimalData() {
+    }
+
+    public void testGetScientificName() {
+    }
+
+    public void testSetScientificName() {
+    }
+
+    public void testGetCommonName() {
+    }
+
+    public void testSetCommonName() {
+    }
+
+    public void testGetFamily() {
+    }
+
+    public void testSetFamily() {
+    }
+
+    public void testGetGender() {
+    }
+
+    public void testSetGender() {
+    }
+}
