18253bc8a1fa8daa9301a263a1cf82c96d280d95
Carolina Melendez <carolina.melendez@bolivia.assuresoft.com>
2024-03-21 08:18:08 -0400

    fix: some wrong asserts

--- src/test/java/assuresoft/com/sciencemuseum2/animals/BatTest.java
+++ src/test/java/assuresoft/com/sciencemuseum2/animals/BatTest.java
@@ -5,13 +5,9 @@ import assuresoft.com.sciencemuseum2.AnimalSystems;
 import assuresoft.com.sciencemuseum2.systems.digestive.CarnivorousDigestion;
 import assuresoft.com.sciencemuseum2.systems.reproductive.SexualReproduction;
 import assuresoft.com.sciencemuseum2.systems.respiratory.LungRespiration;
-import assuresoft.com.sciencemuseum2.systems.sensorial.Sensors;
 import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.List;
-
 import static org.mockito.Mockito.mock;
 
 public class BatTest extends TestCase {
@@ -48,11 +44,6 @@ public class BatTest extends TestCase {
 
     @Test
     public void testBatConstructorWithCorrectSystems() {
-        List<Sensors> mockedSensors = List.of(Sensors.Vision, Sensors.Smell, Sensors.Echolocation);
-        CarnivorousDigestion mockedDigestion = mock(CarnivorousDigestion.class);
-        LungRespiration mockedRespiration = mock(LungRespiration.class);
-        SexualReproduction mockedReproduction = mock(SexualReproduction.class);
-
         String scientificName = "BatScientificName";
         String commonName = "BatCommonName";
         String gender = "Male";
@@ -60,11 +51,10 @@ public class BatTest extends TestCase {
 
         AnimalSystems batSystems = bat.getAnimalSystems();
 
-        // Asserting that AnimalSystems are set properly
-        assertEquals(mockedSensors, batSystems.getSensors());
-        assertEquals(mockedRespiration, batSystems.getRespiration());
-        assertEquals(mockedReproduction, batSystems.getReproduction());
-        assertEquals(mockedDigestion, batSystems.getDigestion());
+        assertTrue(batSystems.getDigestion() instanceof CarnivorousDigestion);
+        assertTrue(batSystems.getReproduction() instanceof SexualReproduction);
+        assertTrue(batSystems.getRespiration() instanceof LungRespiration);
+        assertTrue(batSystems.getSensors() != null);
     }
 
 
